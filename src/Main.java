afaafaddb30a5c64346266e02a7bd30e06c50528
DenizWassmuth <deniz-wassmuth@gmx.de>
2025-12-03 11:44:34 +0100

    add first exercise

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,29 @@
-public class Main {
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main()
+    {
+        printMessage("\nLevel 1 - easy");
+
+        int[] zahlen = new int[5];
+        zahlen[0] = 1;
+        zahlen[1] = 2;
+        zahlen[2] = 3;
+        zahlen[3] = 4;
+        zahlen[4] = 5;
+
+        for(int i = 0; i < zahlen.length; i++)
+        {
+            printMessage("index " + i + " = " + zahlen[i]);
+        }
+    }
+
+    public static void printMessage(String message){
+
+        System.out.println(message);
+    }
 }
+
+
+
