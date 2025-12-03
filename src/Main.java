95bad26128e8783bc2126b98bfde0066f5716034
DenizWassmuth <deniz-wassmuth@gmx.de>
2025-12-03 11:53:42 +0100

    add added second and third exercise

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static void main()
-    {
-        printMessage("\nLevel 1 - easy");
+    static void main() {
+        printMessage("\nLEVEL 1 - EASY\n");
 
         int[] zahlen = new int[5];
         zahlen[0] = 1;
@@ -13,14 +12,32 @@ public class Main
         zahlen[3] = 4;
         zahlen[4] = 5;
 
-        for(int i = 0; i < zahlen.length; i++)
+        for (int i = 0; i < zahlen.length; i++)
         {
             printMessage("index " + i + " = " + zahlen[i]);
         }
+
+        printMessage("\n\n");
+
+        String[] namen = {"Max", "Ben", "Flo", "Ila", "Lia"};
+        for (String s : namen)
+        {
+            printMessage(s);
+        }
+
+        printMessage("\n\n");
+
+        int[] quadratZahlen = {2,4,6,8};
+
+        for (int i = 0; i < quadratZahlen.length; i++)
+        {
+            printMessage("Die Quadratzahl von " + quadratZahlen[i] + " ist " + quadratZahlen[i] + quadratZahlen[i] + "\n");
+        }
+
     }
 
-    public static void printMessage(String message){
-
+    public static void printMessage(String message)
+    {
         System.out.println(message);
     }
 }
